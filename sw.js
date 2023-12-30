2e3016ab0d91fbf73c44c4c06c2a69d04d55a036
Jesus Adolfo Parra Parra <jesus@adolfoparra.com>
2023-12-30 00:28:35 -0500

    primer post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1703905665';
+const cacheName = 'chirpy-1703913972';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
