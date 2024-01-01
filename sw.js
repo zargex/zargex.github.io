2a0a208f24997d39ea0e78f144a5b92f397f8b07
Jesus Adolfo Parra Parra <jesus@adolfoparra.com>
2024-01-01 16:11:30 -0500

    segundo post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1703913972';
+const cacheName = 'chirpy-1704143490';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
