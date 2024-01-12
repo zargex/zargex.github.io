34e35d10a523cb83fb702c9b3108ead47c38f7e0
Jesus Adolfo Parra Parra <jesus@adolfoparra.com>
2024-01-11 22:27:18 -0500

    post sobre instalar cosas en kobo

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704319057';
+const cacheName = 'chirpy-1705030038';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
