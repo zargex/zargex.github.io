3044b6231127f2d835962d19100ca4c57bd81cae
Jesus Adolfo Parra Parra <jesus@adolfoparra.com>
2024-02-12 23:31:28 -0500

    correcciones menores

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707711928';
+const cacheName = 'chirpy-1707798688';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
