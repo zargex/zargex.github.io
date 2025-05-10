f71b321dc16aad8e5b4d59ebc14b9b3ad451632f
Jesus Adolfo Parra Parra <jesus@adolfoparra.com>
2025-05-10 17:20:08 -0400

    changing battery post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707798688';
+const cacheName = 'chirpy-1746912007';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
