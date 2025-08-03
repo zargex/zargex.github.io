2c91ada4f6a4b72a3ea990ecab11aeae53fdc635
Jesus Adolfo Parra Parra <jesus@adolfoparra.com>
2025-08-02 22:41:59 -0400

    arreglando la pagina

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1754187054',resources: [
+    cacheName: 'chirpy-1754188011',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
