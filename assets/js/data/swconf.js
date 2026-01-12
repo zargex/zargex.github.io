00b96477402c6e9189f287beac3022854b780736
Jesus Adolfo Parra Parra <jesus@adolfoparra.com>
2026-01-12 16:35:37 -0500

    updating cv

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1765260918',resources: [
+    cacheName: 'chirpy-1768253737',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
