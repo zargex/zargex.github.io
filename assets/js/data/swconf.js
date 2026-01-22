ecae05d30c56f43259c6926ba550043704f472c6
Jesus Adolfo Parra Parra <jesus@adolfoparra.com>
2026-01-21 23:09:36 -0500

    ceph cert

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1768253737',resources: [
+    cacheName: 'chirpy-1769054976',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
