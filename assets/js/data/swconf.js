d0092bb05a11945e850b56fa541e9af3fd8efa6e
Jesus Adolfo Parra Parra <jesus@adolfoparra.com>
2026-06-14 22:29:29 -0400

    new post for adding shortcuts from Lutris to Steam

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1775088382',resources: [
+    cacheName: 'chirpy-1781490569',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
